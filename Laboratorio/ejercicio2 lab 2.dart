871622776eed9246c59bdfc6c5a772ca5ece2426
N1C0LASr <https://github.com/N1C0LASr/nicol.git>
2023-05-19 11:32:27 -0400

    laboratorio 2 ejercicio dart

---
+++ Laboratorio/ejercicio2 lab 2.dart
@@ -0,0 +1,23 @@
+var numeros = [14,2,5,3,9]; 
+
+var numeros2 = [1,2,3,4,5];
+
+var numeros3 = [-15,-16,-17,-18,-19];
+
+void main () (
+  var numeros = [14,2,5,3,9];
+  var numeros2 = [1,2,3,4,5];
+
+  int resta =0;
+  for (numero in numeros2) (
+    resta -= numero,numeros2;
+  )
+  print (resta)
+),;
+///
+///
+///
+///
+
+
+
