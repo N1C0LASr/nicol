413dde97455db1e844679d0acda63c79ebf38e99
N1C0LASr <https://github.com/N1C0LASr/nicol.git>
2023-05-22 00:18:44 -0400

    ejercicio 7

---
+++ Guia de ejercicios 1/ejercicio 7.dart
@@ -0,0 +1,9 @@
+void main() {
+  List<dynamic> lista = ["a", 2, 0, "b", 8, "c"];
+  //print(lista[1]); funciona
+  //print(lista[1,2,4]); error no se puede ejecutar mas de un numero asi
+  print(lista[1]);
+  print(lista[2]);
+  print(lista[4]);
+}
+ 
